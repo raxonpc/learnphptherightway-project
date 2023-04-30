9e1d5b3dc5ae017f31bf8176e25395ff3776dfe3
raxonpc <tomeczekorlowski1@gmail.com>
2023-04-30 15:44:41 +0200

    Implement function to read csv files to an array

--- app/App.php
+++ app/App.php
@@ -1,5 +1,27 @@
 <?php
 
-declare(strict_types = 1);
+declare(strict_types=1);
 
 // Your Code
+function get_transactions(string $file_path): array|null
+{
+    if (!is_file($file_path)) {
+        return null;
+    }
+
+    $file = fopen($file_path, "r");
+    if ($file === false) {
+        return null;
+    }
+
+    $rows = [];
+    $row_count = 0;
+    while (($row = fgetcsv($file)) !== false) {
+        $row_count = array_push($rows, $row);
+    }
+    return $row_count === 0 ? null : $rows;
+}
+
+echo "<pre>\n";
+print_r(get_transactions(FILES_PATH . "sample_1.csv"));
+echo "</pre>\n";
